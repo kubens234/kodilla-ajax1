640f8afa83eaf9b0e41cb68ac59a21d8a4b43400
jakub <mroczkowski.jakub84@gmail.com>
2016-12-29 16:53:11 +0100

    poprawione

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 "use strict";
+
 window.onload = function() {
 	getJoke();
 };
+
 var url = 'http://api.icndb.com/jokes/random',
 	button = document.getElementById('get-joke'),
 	paragraph = document.getElementById('joke');
 	
 button.addEventListener('click', function(){
-  getJoke();
+	getJoke();
 });
 
 function getJoke() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url);
 	xhr.addEventListener('load', function(){
-  	var response = JSON.parse(xhr.response);
-    paragraph.innerText = response.value.joke;
+  		var response = JSON.parse(xhr.response);
+    	paragraph.innerText = response.value.joke;
 	});
 	xhr.send();
 }
